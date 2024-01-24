679d50cfa03ac869b43cf0ce73af57ec9eea4728
quagh03 <03.quanghuy.le@gmail.com>
2024-01-24 18:31:24 +0700

    JwtTokenUtil init

---
+++ src/main/java/org/quagh/blogbackend/components/JwtTokenUtil.java
@@ -0,0 +1,65 @@
+package org.quagh.blogbackend.components;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.quagh.blogbackend.entities.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenUtil {
+    @Value("${jwt.expiration}")
+    private int expiration;
+
+    @Value("${jwt.secretKey}")
+    private String secretKey;
+    public String generateToken(User user){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("username", user.getUsername());
+        try {
+            return Jwts.builder()
+                    .setClaims(claims)
+                    .setSubject(user.getUsername())
+                    .setExpiration(new Date(System.currentTimeMillis() + expiration  * 1000L))
+                    .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                    .compact();
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+    private Key getSignInKey(){
+        byte[] bytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(bytes);
+    }
+
+    private Claims extractAllClaim(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = this.extractAllClaim(token);
+        return claimsResolver.apply(claims);
+    }
+
+    //Check expiration
+    public boolean isTokenExpired(String token){
+        Date expirationDate = this.extractClaim(token, Claims::getExpiration);
+        return expirationDate.before(new Date());
+    }
+}
